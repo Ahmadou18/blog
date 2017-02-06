6ec7dbcf9e52c666be67a39d036be5ac3bb3873a
GUEYE Ahmadou <ah.gueye@laposte.net>
2017-02-06 22:08:46 +0100

    blog

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <title>Blog</title>
+</head>
+<body>
+  <header class="head">
+    <nav>
+      <ul class="menu">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+  <section class="container">
+    <div class="index">
+        <p>Commentaire du jour</p>
+    </div>
+    <aside class="post">
+      <p>Commentaires précédents</p>
+    </aside>
+  </section>
+</body>
+</html>
+  
