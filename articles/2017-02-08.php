331aa2bfce1928c3ad2382f5c8f00e7920763b9c
ahmadou18 <ah.gueye@laposte.net>
2017-02-09 22:29:37 +0100

    ajout bouton suivant précédent

---
+++ articles/2017-02-08.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>j'ai un peu de mal avec le php mais je commence à comprendre petit à petit</p>
+
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+
+<?php
+$entries = array_diff (scandir("articles", SCANDIR_SORT_DESCENDING), array(".", ".."));
+$myarticle = array_search($_GET['page'], $entries);
+$beforePosition = $myarticle - 1;
+$afterPosition = $myarticle + 1;
+$before = $entries[$beforePosition];
+$after = $entries[$afterPosition];
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,22 +17,75 @@
   <title>Blog</title>
 </head>
 <body>
-  <header class="head">
-    <nav>
-      <ul class="menu">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
-  </header>
-  <section class="container">
-    <div class="index">
-        <p>Commentaire du jour</p>
-    </div>
-    <aside class="post">
-      <p>Commentaires précédents</p>
-    </aside>
-  </section>
-</body>
-</html>
-  
+
+  <main class="img">
+    <header class="head">
+      <nav>
+        <ul class="menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <section class="container">
+      <aside class="post">
+        <ol>
+
+          <?php
+
+          foreach ($entries as $entry)
+          {
+            if ($entry != ".." && $entry != ".")
+            {
+              $rest = pathinfo($entry);
+
+
+
+              echo '<li><a href="index.php?page=' . $entry .'">' . $rest['filename'] . '</a></li>';
+            }
+          }
+          ?>
+
+
+
+        </ol>
+      </aside>
+      <article class="index">
+
+        <?php include ('articles/') . $_GET["page"]; ?>
+        <?php echo "<a href='index.php?page=$before'
+        >Précèdent</a>";
+        
+        ?>
+        <?php echo "<a href='index.php?page=$after'
+        >Suivant</a>" ?>
+
+
+
+      </article>
+    </section>
+  </main>
+  <?php
+
+  $articles_dir = "articles";
+  $show_article = false;
+
+  if (isset($_GET['wanted']))
+  {
+    $article_path = "$articles_dir/" . $_GET["wanted"] . ".php";
+    if (
+      dirname(
+        realpath($article_path)
+        ) == (
+          realpath("./$articles_dir")
+          )
+          )
+          {
+            $show_article = true;
+            include($article_path);
+
+          }
+        }
+        ?>
+      </body>
+      </html>
